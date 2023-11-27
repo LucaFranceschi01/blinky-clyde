c43356cecb5467f5aeeeab5051fa2d61e5bde52d
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2023-11-27 19:51:09 +0100

    Some risky changes

--- myTeam.py
+++ myTeam.py
@@ -1,11 +1,10 @@
-from myutils import PacmanQAgent, SimpleExtractor
-
-from captureAgents import CaptureAgent
-from game import Directions
+from capture import GameState
+from myutils import PacmanQAgent
 from util import nearestPoint
 import random
 import util
 import json
+from myutils import Actions, closestFood, closestEnemy, closestScaredGhost
 
 #################
 # Team creation #
@@ -31,22 +30,22 @@ def create_team(first_index, second_index, is_red,
 
 class ApproximateQAgent(PacmanQAgent):
     def __init__(self, index, time_for_computing=.1, **args):
-        self.featExtractor = SimpleExtractor()
         PacmanQAgent.__init__(self, index, time_for_computing, **args)
 
-        with open('src/contest/agents/blinky-clyde/weights.txt', 'r') as fin:
+        with open('agents/blinky-clyde/weights.txt', 'r') as fin: # habrá que cambiarlo por solo weights.txt
             raw_weights = fin.read()
-        
+
         self.weights = util.Counter(json.loads(raw_weights))
         
         self.start = None
-        # {'bias':0, '#_of_enemies_at_1_step':0,'dist_closest_food': 0.0, 'x_pos': 0, 'y_pos':0, 'dist_closest_enemy':0, 'dist_closest_vulnerable_enemy':0, 'food_carrying':0, }
+        # {"bias":0, "#_of_enemies_at_1_step":0, "#_of_enemies_at_2_step":0, "dist_closest_food": 0.0, "x_pos": 0, "y_pos":0, "dist_closest_enemy":0, "dist_closest_vulnerable_enemy":0, "food_carrying":0}
+
 
     def register_initial_state(self, game_state):
         self.start = game_state.get_agent_position(self.index)
         PacmanQAgent.register_initial_state(self, game_state)
 
-    def computeActionFromQValues(self, state):
+    def computeActionFromQValues(self, game_state):
         """
           Compute the best action to take in a state.  Note that if there
           are no legal actions, which is the case at the terminal state,
@@ -54,15 +53,15 @@ class ApproximateQAgent(PacmanQAgent):
         """
         "*** YOUR CODE HERE ***"
 
-        legalActions = self.getLegalActions(state)
+        legalActions = game_state.get_legal_actions(self.index)
      
         if len(legalActions) == 0:
             return None
         
-        #Compute maximum Q-value for each possible action of a state, and return the value and the action taken
+        #Compute maximum Q-value for each possible action of a game_state, and return the value and the action taken
         q_max = [float('-inf'), legalActions[0]]
         for a in legalActions: 
-            q_max = max(q_max, [self.getQValue(state, a), a], key=lambda x:x[0])
+            q_max = max(q_max, [self.getQValue(game_state, a), a], key=lambda x:x[0])
         return q_max[1]
     
     def getWeights(self): # gives some strange error
@@ -73,63 +72,38 @@ class ApproximateQAgent(PacmanQAgent):
           Should return Q(state,action) = w * featureVector
           where * is the dotProduct operator
         """
-        "*** YOUR CODE HERE ***"
-        return self.weights * self.weights # * self.featExtractor.getFeatures(state, action)
-	# es temporal hasta que tengamos las features hechas 
+        features = self.get_features(state, action)
+        weights = self.get_weights(state, action)
+        return features * weights
 
     def update(self, state, action, nextState, reward, episode):
         """
            Should update your weights based on transition
         """
-        "*** YOUR CODE HERE ***"
         delta = (float(reward) + self.discount*self.computeValueFromQValues(nextState)) - self.getQValue(state, action)
-        for key in self.featExtractor.getFeatures(state, action):
-            self.weights[key] += self.alpha * delta * self.featExtractor.getFeatures(state, action)[key]
+        for key in self.get_features(state, action):
+            self.weights[key] += self.alpha * delta * self.get_features(state, action)[key]
 
         if episode % 2 == 0: # 2 es temporal
-            with open('src/contest/agents/blinky-clyde/weights_out.txt', 'w') as fout: 
+            with open('agents/blinky-clyde/weights_out.txt', 'w') as fout: 
                 fout.write(json.dumps(self.weights))
 
-    def get_action(self, state):
-        legalActions = self.getLegalActions(state)
+    def get_action(self, game_state):
+        return self.choose_action(game_state)
+
+    def choose_action(self, game_state):
+        legalActions = game_state.get_legal_actions(self.index)
         action = None
 
         if len(legalActions) == 0:
           return action
         
-        action = self.computeActionFromQValues(state) #Take best policy action
+        if random.random() < 0.5:
+            action = self.computeActionFromQValues(game_state) #Take best policy action
+        else:
+            action = random.choice(legalActions)
         
         return action
-    
-    def choose_action(self, game_state):
-        """
-        Picks among the actions with the highest Q(s,a).
-        """
-        actions = game_state.get_legal_actions(self.index)
-
-        # You can profile your evaluation time by uncommenting these lines
-        # start = time.time()
-        values = [self.evaluate(game_state, a) for a in actions]
-        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
-
-        max_value = max(values)
-        best_actions = [a for a, v in zip(actions, values) if v == max_value]
-
-        food_left = len(self.get_food(game_state).as_list())
-
-        if food_left <= 2:
-            best_dist = 9999
-            best_action = None
-            for action in actions:
-                successor = self.get_successor(game_state, action)
-                pos2 = successor.get_agent_position(self.index)
-                dist = self.get_maze_distance(self.start, pos2)
-                if dist < best_dist:
-                    best_action = action
-                    best_dist = dist
-            return best_action
-
-        return random.choice(best_actions)
 
     def get_successor(self, game_state, action):
         """
@@ -143,26 +117,78 @@ class ApproximateQAgent(PacmanQAgent):
         else:
             return successor
 
-    def evaluate(self, game_state, action):
-        """
-        Computes a linear combination of features and feature weights
-        """
-        features = self.get_features(game_state, action)
-        weights = self.get_weights(game_state, action)
-        return features * weights
-
     def get_features(self, game_state, action):
         """
         Returns a counter of features for the state
         """
+        # features = util.Counter()
+        # successor = self.get_successor(game_state, action)
+        # features['successor_score'] = self.get_score(successor)
+        
+        # Initialize helpful variables
         features = util.Counter()
-        successor = self.get_successor(game_state, action)
-        features['successor_score'] = self.get_score(successor)
+        if game_state.is_on_red_team(self.index):
+            enemy_food = game_state.get_blue_food()
+            team_food = game_state.get_red_food()
+            enemies = game_state.get_blue_team_indices()
+        else:
+            enemy_food = game_state.get_red_food()
+            team_food = game_state.get_blue_food()
+            enemies = game_state.get_red_team_indices()
+
+        walls = game_state.get_walls()
+        score = game_state.get_score()
+
+        ####################################       
+        enemy_positions = [] 
+        for enemy in enemies:
+            enemy_positions.append(game_state.get_agent_position(enemy)) # uno cualquiera, pero tiene que ser del equipo contrario o algo asi
+
+        # Current and future locations of the agent
+        x, y = game_state.get_agent_position(self.index)
+        dx, dy = Actions.direction_to_vector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+        next_x_2, next_y_2 = int(x + 2*dx), int(y + 2*dy) # NO ME CONVENCE, son 2 steps en linea recta, no en otras direcciones que también estén a 2 steps
+
+        # FEATURE 1: BIAS
+        features["bias"] = 1.0
+        
+        # FEATURE 2: NUMBER OF ENEMIES AT ONE STEP
+        features["#-of-enemies-1-step-away"] = sum([(next_x, next_y) == game_state.get_legal_actions(enemy) for enemy in enemies])
+
+        # FEATURE 3: NUMBER OF ENEMIES TWO STEPS AWAY
+        features["#-of-enemies-2-step-away"] = sum([(next_x_2, next_y_2) == game_state.get_legal_actions(enemy) for enemy in enemies])
+
+        # FEATURE 4: NUMBER OF SCARED ENEMIES ONE STEP AWAY
+        features["#-of-scared-enemies-1-step-away"] = sum([(next_x, next_y) == game_state.get_legal_actions(enemy) and 
+                                                          game_state.get_agent_state(enemy).scared_timer > 1 for enemy in enemies])
+
+        # FEATURES 5 AND 6: EAT FOOD OR ENEMY
+        features["eats-food"] = features["eats-enemy"] = 0.0
+        if features["#-of-scared-enemies-1-step-away"] > 0:
+            features["eats-enemy"] = 1.0
+        elif features["#-of-enemies-1-step-away"] == 0 and (next_x, next_y) in enemy_food:
+            features["eats-food"] = 1.0
+
+        # FEATURE 7: DISTANCE TO CLOSEST FOOD
+        dist_food = closestFood((next_x, next_y), enemy_food, walls)
+        if dist_food is not None:
+            # make the distance a number less than one otherwise the update will diverge wildly
+            features["dist_closest_food"] = float(dist_food) / (walls.width * walls.height)
+        # features.divideAll(10.0)
+
+        # FEATURES 8 AND 9: POSITION IN THE BOARD
+        features["x_pos"] = next_x / walls.width - 0.5 # Normalized position in the board, with 0 in the center
+        features["y_pos"] = next_y / walls.height
+
+        # FEATURE 10: FOOD_CARRYING
+        features["food_carrying"] = game_state.get_agent_state(enemy).num_carrying
+
         return features
 
     def get_weights(self, game_state, action):
         """
-        Normally, weights do not depend on the game state.  They can be either
+        Normally, weights do not depend on the game game_state.  They can be either
         a counter or a dictionary.
         """
-        return {'successor_score': 1.0}
+        return self.weights
--- myutils.py
+++ myutils.py
@@ -4,30 +4,6 @@ import util
 import time
 import json
 
-class FeatureExtractor:
-    def getFeatures(self, state, action):
-        """
-          Returns a dict from features to counts
-          Usually, the count will just be 1.0 for
-          indicator functions.
-        """
-        util.raiseNotDefined()
-
-class IdentityExtractor(FeatureExtractor):
-    def getFeatures(self, state, action):
-        feats = util.Counter()
-        feats[(state,action)] = 1.0
-        return feats
-
-class CoordinateExtractor(FeatureExtractor):
-    def getFeatures(self, state, action):
-        feats = util.Counter()
-        feats[state] = 1.0
-        feats['x=%d' % state[0]] = 1.0
-        feats['y=%d' % state[0]] = 1.0
-        feats['action=%s' % action] = 1.0
-        return feats
-
 def closestFood(pos, food, walls):
     """
     closestFood -- this is similar to the function that we have
@@ -51,7 +27,7 @@ def closestFood(pos, food, walls):
     return None
 
 #TO DO
-def closestGhost(pos, blue_ghost, walls):
+def closestEnemy(pos, enemies, game_state):
     """
     Return closest ghost to our position
     """
@@ -62,11 +38,12 @@ def closestGhost(pos, blue_ghost, walls):
         if (pos_x, pos_y) in expanded:
             continue
         expanded.add((pos_x, pos_y))
-        # if we find a ghost at this location then exit
-        if blue_ghost[pos_x][pos_y]:
-            return dist
+        # if we find an enemy at this location then exit
+        for enemy in enemies:
+            if game_state.get_agent_position(enemy) == (pos_x, pos_y):
+                return dist
         # otherwise spread out from the location to its neighbours
-        nbrs = Actions.get_legal_neighbors((pos_x, pos_y), walls)
+        nbrs = Actions.get_legal_neighbors((pos_x, pos_y), game_state.get_walls())
         for nbr_x, nbr_y in nbrs:
             fringe.append((nbr_x, nbr_y, dist+1))
     # no ghost found
@@ -95,100 +72,6 @@ def closestScaredGhost(pos, blue_scared_ghost, walls):
     # no scared ghost found
     return None
 
-
-class SimpleExtractor(FeatureExtractor):
-    """
-    Returns simple features for a basic reflex Pacman:
-    - whether food will be eaten
-    - how far away the next food is
-    - whether a ghost collision is imminent
-    - whether a ghost is one step away
-    """
-
-    def getFeatures(self, state, action):
-        # extract the grid of food and wall locations and get the ghost locations
-        food = state.get_red_food()
-        walls = state.get_walls()
-
-        ####################################       
-        ghosts = state.get_agent_position(state.blue_team[0]) # uno cualquiera, pero tiene que ser del equipo contrario o algo asi
-        scared_ghosts = state.get_agent_position(state.blue_team[0]) ##HOW TO DIFFERENTIATE BOTH GHOSTS
-        ####################################
-        
-        features = util.Counter()
-
-        features["bias"] = 1.0
-
-        # compute the location of pacman after he takes the action
-        x, y = state.get_agent_position(state.red_team[0])
-        dx, dy = Actions.direction_to_vector(action)
-        next_x, next_y = int(x + dx), int(y + dy)
-        next_x_2, next_y_2 = int(x + 2*dx), int(y + 2*dy)
-
-        
-        # FEATURE 1: NUMBER OF ACTIVE GHOST ONE STEP AWAY
-        #count the number of ghosts 1-step away
-        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.get_legal_neighbors(g, walls) for g in ghosts)
-
-        #FEATURE 2: NUMBER OF ACTIVE GHOST TWO STEPS AWAY
-        # count the number of ghosts 1-step away
-        features["#-of-ghosts-2-step-away"] = sum((next_x_2, next_y_2) in Actions.get_legal_neighbors(g, walls) for g in ghosts)
-
-        #FEATURE 3: EAT FOOD IF NO ACTIVE GHOSTS NEAR
-        # if there is no danger of ghosts then add the food feature
-        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
-            features["eats-food"] = 1.0
-
-        #FEATURE 4: DISTANCE TO CLOSEST FOOD
-        dist_food = closestFood((next_x, next_y), food, walls)
-        if dist_food is not None:
-            # make the distance a number less than one otherwise the update
-            # will diverge wildly
-            features["dist_closest_food"] = float(dist_food) / (walls.width * walls.height)
-        features.divideAll(10.0)
-
-        #FEATURE 5: POSITION IN THE BOARD
-        #"x_pos": 0, "y_pos":0
-        features["x_pos"] = next_x
-        features["y_pos"] = next_y
-
-        #FEATURE 6: DISTANCE TO CLOSEST GHOST
-        #Implementar closestGhost
-        dist_ghost = closestGhost((next_x, next_y), ghosts, walls)
-        if dist_ghost is not None:
-            # make the distance a number less than one otherwise the update
-            # will diverge wildly
-            features["dist_closest_enemy"] = float(dist_ghost) / (walls.width * walls.height)
-        features.divideAll(10.0)
-        
-        
-        #FEATURE 7: DISTANCE TO CLOSEST SCARED GHOST
-        #Implementar closestScaredGhost
-        dist_scared_ghost = closestScaredGhost((next_x, next_y), scared_ghosts, walls)
-        if dist_scared_ghost is not None:
-            # make the distance a number less than one otherwise the update
-            # will diverge wildly
-            features["dist_closest_vulnerable_enemy"] = float(dist_scared_ghost) / (walls.width * walls.height)
-        features.divideAll(10.0)
-
-        #FEATURE 8: FOOD_CARRYING
-        #features["food_carrying"] = 
-
-
-        # FEATURE 9: NUMBER OF SCARED GHOST ONE STEP AWAY
-        #count the number of ghosts 1-step away
-        features["#-of-scared-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.get_legal_neighbors(g, walls) for g in scared_ghosts)
-
-        #FEATURE 10: NUMBER OF SCARED GHOST TWO STEPS AWAY
-        # count the number of ghosts 1-step away
-        features["#-of-scared-ghosts-2-step-away"] = sum((next_x_2, next_y_2) in Actions.get_legal_neighbors(g, walls) for g in scared_ghosts)
-
-
-        #FEATURE 11: DISTANCE TO CAPSULES??
-
-
-        return features
-        
 class ValueEstimationAgent(Agent):
     """
       Abstract agent which assigns values to (state,action)
