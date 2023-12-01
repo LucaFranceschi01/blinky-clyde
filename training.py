a2d7089bdfb1753fad043809c8e423ff9a51e11a
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2023-12-01 11:09:46 +0100

    upload training.py

--- training.py
+++ training.py
@@ -1,17 +1,14 @@
-from capture import GameState
-from myutils import PacmanQAgent
-import util
+import capture
+import os
+import sys
 
-def training_agent():
-    num_training = 10
-    pacman = PacmanQAgent()
+def main():
+	for _ in range(1000000):
+		try:
+			capture.run(sys.argv[1:])
+		except TypeError:
+			break
+		# python train.py -r agents/blinky-clyde/myTeam.py -b agents/team_template/myTeam.py --time 10000
 
-
-    for train in range(num_training):
-        state = GameState()
-        pacman.register_initial_state(state)
-
-        while not state.is_over():
-            action = pacman.getAction(state)
-            state = state.generate_successor(pacman.index, action)
-        pacman.final(state)
+if __name__ == '__main__':
+	main()
