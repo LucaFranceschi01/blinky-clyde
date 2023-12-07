b808149e936c3ec0394b23c334eecf61f2cdfb0d
CandelaAlvLop <candela.alvarez02@estudiant.upf.edu>
2023-12-07 14:35:30 +0100

    Finnished

--- TEAM.md
+++ TEAM.md
@@ -6,11 +6,9 @@
 
 **Instructor:** Sergio Calo
 
-**Team name:** blinky&clyde
+**Team name:** blinky-clyde
 
 **Team members:**
 
 * 199149 - Luca Franceschi - luca.franceschi01@estudiant.upf.edu - LucaFranceschi01
 * 198640 - Candela Álvarez López - candela.alvarez02@estudiant.upf.edu - CandelaAlvLop
-
-Vamos a probar con el approach de q-learning
--- myutils.py
+++ myutils.py
@@ -12,13 +12,21 @@ def closest_food(approxQagent, pos, food):
                 dist_food = min(dist_food, [approxQagent.get_maze_distance(pos, (i, j)), (i, j)], key=lambda x:x[0])
     return dist_food
 
+
 def closest_capsule(approxQagent, pos, capsules):
+    '''
+    Returns distance and position of the closest capsule found from the position of the agent.
+    '''
     dist_capsule = float('inf')
     for c in capsules:
         dist_capsule = min(dist_capsule, approxQagent.get_maze_distance(pos, c))
     return dist_capsule
 
+
 def count_food(food):
+    '''
+    Returns the number of food left.
+    '''
     food_count = 0
     for i in range(food.width):
         for j in range(food.height):
